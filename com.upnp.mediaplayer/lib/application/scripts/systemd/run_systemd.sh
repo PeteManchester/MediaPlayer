ee678a67957587c29c997305146d8cb0757480ab
Markus M. May <mmay@javafreedom.org>
2014-03-25 12:10:45 +0100

    Adopt systemd startup files

--- com.upnp.mediaplayer/lib/application/scripts/systemd/run_systemd.sh
+++ com.upnp.mediaplayer/lib/application/scripts/systemd/run_systemd.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
-DIRNAME="$( dirname "$0" )"
-cd "${DIRNAME}"
-exec java -jar "${DIRNAME}"/mediaplayer.jar
+# Absolute path to this script, e.g. /home/pi/mediaplayer/run.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/pi/mediaplayer
+SCRIPTPATH=$(dirname "$SCRIPT")
 
+cd "${SCRIPTPATH}"
+echo ${SCRIPTPATH}
 
+java -jar "${SCRIPTPATH}"/mediaplayer.jar > /dev/null &
